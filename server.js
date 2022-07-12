d5dd54b548837b52e1f1fa2a054d1ea6058fc9d1
alik.ragimov <ragimovalik@gmail.com>
2022-07-12 15:37:39 +0300

    Dependencies updated, swagger added

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ const fastify = require('fastify')({ logger: true });
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+fastify.register(require('@fastify/swagger'), {
+  routePrefix: '/documentation',
+  swagger: {
+    info: {
+      title: 'Delivery App',
+      description: 'Delivery App Api Documentation',
+      version: '0.1.0',
+    },
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+  },
+  exposeRoute: true,
+});
+// fastify.swagger();
+
 fastify.register(require('@fastify/cors'));
 
 const { PORT = 3040, DB_HOST } = process.env;
